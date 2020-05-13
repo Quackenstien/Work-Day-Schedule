65b62ad267669b85f70908baf021c4c01df89f7a
Quackenstien <donaldhawkins422@gmail.com>
2020-05-13 14:36:39 -0700

    added some logic for the text area colors to change based on time of day

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 const date = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(date);
+
+$(".time-block").each(function () {
+  const currentHour = parseInt(moment().format("H"));
+  const hour = parseInt($(this).attr("data-hour"));
+
+  if (hour < currentHour) {
+    $(this).find("textarea").addClass("past");
+  } else if (hour > currentHour) {
+    $(this).find("textarea").addClass("future");
+  } else {
+    $(this).find("textarea").addClass("present");
+  }
+
+  console.log(hour, currentHour);
+});
