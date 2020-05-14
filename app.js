977cc4a1aabec5350e902076105e451063f9f9d5
Quackenstien <donaldhawkins422@gmail.com>
2020-05-14 09:39:08 -0700

    set up function to save text into local to storage

--- app.js
+++ app.js
@@ -15,3 +15,21 @@ $(".time-block").each(function () {
 
   console.log(hour, currentHour);
 });
+
+$(".saveBtn").on("click", function () {
+  event.preventDefault();
+
+  var time = $(this).parent().attr("data-hour");
+  var value = $(this).siblings(".description").val();
+  localStorage.setItem(time, value);
+});
+
+$("#hr9 .description").val(localStorage.getItem("9"));
+$("#hr10 .description").val(localStorage.getItem("10"));
+$("#hr11 .description").val(localStorage.getItem("11"));
+$("#hr12 .description").val(localStorage.getItem("12"));
+$("#hr13 .description").val(localStorage.getItem("13"));
+$("#hr14 .description").val(localStorage.getItem("14"));
+$("#hr15 .description").val(localStorage.getItem("15"));
+$("#hr16 .description").val(localStorage.getItem("16"));
+$("#hr17 .description").val(localStorage.getItem("17"));
